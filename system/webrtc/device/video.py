f17ae71ca1bcb7daade2a52c2732936fdbbaa418
stefpi <19478336+stefpi@users.noreply.github.com>
2026-03-11 19:46:05 -0700

    bitrate and speed increases

--- system/webrtc/device/video.py
+++ system/webrtc/device/video.py
@@ -23,12 +23,15 @@ class LiveStreamVideoStreamTrack(TiciVideoStreamTrack):
     self._pts = 0
     self._t0_ns = time.monotonic_ns()
 
-  async def recv(self):
+  def _recv_blocking(self):
     while True:
-      msg = messaging.recv_one_or_none(self._sock)
+      msg = messaging.recv_one(self._sock)
       if msg is not None:
-        break
-      await asyncio.sleep(0.005)
+        return msg
+
+  async def recv(self):
+    loop = asyncio.get_running_loop()
+    msg = await loop.run_in_executor(None, self._recv_blocking)
 
     evta = getattr(msg, msg.which())
 
